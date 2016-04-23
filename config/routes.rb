a9c240786bc5d61ee6ee0c8bfbcd8abcad2b7198
Alyssa Ross <hi@alyssa.is>
2016-04-23 13:38:48 +0100

    Remove locations#show route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :items
-  resources :locations, only: [:index, :show]
+  resources :locations, only: [:index]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
