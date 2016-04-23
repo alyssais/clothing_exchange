2dfb5d329eccfe83de8a1d581ce12d5b79317412
Alyssa Ross <hi@alyssa.is>
2016-04-23 10:30:28 +0100

    Create items

---
+++ app/views/items/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @item, :id, :title, :description, :email, :created_at, :updated_at
---
+++ db/migrate/20160423092747_create_items.rb
@@ -0,0 +1,11 @@
+class CreateItems < ActiveRecord::Migration
+  def change
+    create_table :items do |t|
+      t.text :title
+      t.text :description
+      t.text :email
+
+      t.timestamps null: false
+    end
+  end
+end
