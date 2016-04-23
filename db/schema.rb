730dd538981bbd81dda37677a37d20798161ab7c
Alyssa Ross <hi@alyssa.is>
2016-04-23 10:50:48 +0100

    Add locations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160423092747) do
+ActiveRecord::Schema.define(version: 20160423094804) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20160423092747) do
     t.text     "email"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.text     "location"
   end
 
 end
